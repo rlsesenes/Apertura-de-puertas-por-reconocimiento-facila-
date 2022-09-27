40fdbec9b13d89915946a831cd5c2fc84f55ecc6
rlsesenes <112500696+rlsesenes@users.noreply.github.com>
2022-09-27 11:21:33 -0500

    segundo analyses

--- DeepFace/Ejemplos/face-check.py
+++ DeepFace/Ejemplos/face-check.py
@@ -3,7 +3,7 @@ from deepface import DeepFace
 print ("Se esta comparando dos imagenes para verificar identidad")
 print ( "cargando...")
 
-result = DeepFace.verify(img1_path = "/home/sesenes/Documents/GitHub/Apertura-de-puertas-por-reconocimiento-facila-/DeepFace/Faces/demi_1.jpeg", img2_path = "/home/sesenes/Documents/GitHub/Apertura-de-puertas-por-reconocimiento-facila-/DeepFace/Faces/Demi_2.jpeg")
-
+# result = DeepFace.verify(img1_path = "/home/sesenes/Documents/GitHub/Apertura-de-puertas-por-reconocimiento-facila-/DeepFace/Faces/demi_1.jpeg", img2_path = "/home/sesenes/Documents/GitHub/Apertura-de-puertas-por-reconocimiento-facila-/DeepFace/Faces/Demi_2.jpeg")
+result = DeepFace.verify(img1_path = "/home/sesenes/Documents/GitHub/Apertura-de-puertas-por-reconocimiento-facila-/DeepFace/Faces/demi_1.jpeg", img2_path = "/home/sesenes/Documents/GitHub/Apertura-de-puertas-por-reconocimiento-facila-/DeepFace/Faces/nicole.jpeg")
 print("estos son los resultados:")
 print (result)
