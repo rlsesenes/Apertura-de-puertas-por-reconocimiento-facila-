8672e1012be41702d90dc900b74be8db1cfa220b
rlsesenes <112500696+rlsesenes@users.noreply.github.com>
2022-09-29 10:26:05 -0500

    cambios realizados

--- deepface/Ejemplos/face_recognition.py
+++ deepface/Ejemplos/face_recognition.py
@@ -1,5 +1,13 @@
-df = DeepFace.find(img_path = "img1.jpg", db_path = "C:/workspace/my_db")
-df = DeepFace.find (img_path = "/home/sesenes/Documents/GitHub/Apertura-de-puertas-por-reconocimiento-facila-/DeepFace/Faces/NICOLEKIDMAN.jpg", db_path = "/home/sesenes/Documents/GitHub/Apertura-de-puertas-por-reconocimiento-facila-/DeepFace/my_db", enforce_detection = "false")
+#Importar biblioteca
+
+from deepface import DeepFace
+import pandas as pd
+
+# buscar rostro
+print ("buscando rostro")
+
+#df = DeepFace.find(img_path = "img1.jpg", db_path = "C:/workspace/my_db")
+df = DeepFace.find (img_path = "/home/sesenes/Documents/GitHub/Apertura-de-puertas-por-reconocimiento-facila-/deepface/Faces/NICOLEKIDMAN.jpg", db_path = "/home/sesenes/Documents/GitHub/Apertura-de-puertas-por-reconocimiento-facila-/deepface/my_db", enforce_detection = "false")
 print ("Resultado ")
 print (df)
 print ("Seleccion")
